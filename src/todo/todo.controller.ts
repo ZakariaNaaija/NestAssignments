ec66c2ce9ab50b71a43cd7d66650ce4d1e4669bf
ZakariaNaaija <zakarianaaija@gmail.com>
2020-12-17 22:22:54 +0100

    added typeORM

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { CreateTodoDto } from './dto/create-todo.dto';
+import { CritereDto } from './dto/critere.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { TodoEntity } from './entities/todo.entity';
 import { TodoService } from './todo.service';
 
 @Controller('todo')
@@ -8,8 +10,6 @@ export class TodoController {
     constructor(private todoService: TodoService) {
 
     }
-
-
     @Post()
     addTodo(
         @Body() newTodo: CreateTodoDto
@@ -18,9 +18,16 @@ export class TodoController {
         return this.todoService.addTodo(newTodo);
     }
 
+    @Get('/search')
+    searchTodo(
+        @Body() critereTodo: CritereDto
+    ) {
+        return this.todoService.searchTodo(critereTodo);
+    }
+
     @Get()
-    getTodos() {
-        return this.todoService.getTodos();
+    getTodos():Promise<TodoEntity[]> {
+        return this.todoService.findAllTodo();
     }
 
     @Get(':id')
@@ -30,16 +37,23 @@ export class TodoController {
         return this.todoService.findTodoById(id);
     }
 
+    @Get('restore/:id')
+    restoreTodoById(
+        @Param('id') id
+    ) {
+        return this.todoService.restoreTodo(id);
+    }
+
     @Put(':id')
     updateTodo(
         @Param('id') id,
         @Body() newTodo: UpdateTodoDto
-    ) {
+    ):Promise<TodoEntity> {
         return this.todoService.updateTodo(id, newTodo);
     }
 
     @Delete(':id')
-    deleteTodo(@Param('id') id) {
+    deleteTodo(@Param('id') id:number) {
         return this.todoService.deleteTodo(id);
     }
 
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,13 +1,21 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { In, Like, Repository } from 'typeorm';
 import { CreateTodoDto } from './dto/create-todo.dto';
+import { CritereDto } from './dto/critere.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { TodoEntity } from './entities/todo.entity';
+import { TodoStatusEnum } from './enum/todo-status.enum';
 import { Todo } from './models/todo.model';
 
 @Injectable()
 export class TodoService {
     private todos : Todo[] = [];
 
-    constructor() {
+    constructor(
+        @InjectRepository(TodoEntity)
+        private readonly TodoRepository:Repository<TodoEntity>
+    ) {
         this.todos = [
             new Todo(),
             new Todo()
@@ -16,38 +24,54 @@ export class TodoService {
     getTodos(): Todo[] {
         return this.todos;
     }
-
-    findTodoById(id : string){
-        const todo = this.todos.find(
-            (actualTodo)=> actualTodo.id ===id
-        );
-        if(todo)
-            return todo;
-        throw new NotFoundException('TODO NOT FOUND');
-
+    
+    async searchTodo(critereTodo:CritereDto) {
+        const todos = await this.TodoRepository.find(
+        {
+          description:Like(`%${critereTodo.chaine}%`),
+          statut:In([TodoStatusEnum.actif,TodoStatusEnum.waiting,TodoStatusEnum.actif])        
+        });
+        if (todos[0])
+          return todos[0];
+        throw new NotFoundException(`Le todo avec ce critére n'existe pas`);
     }
 
-    addTodo(newTodo : CreateTodoDto) {
-        const {name, description } = newTodo;
-        const todo = new Todo();
-        todo.name = name;
-        todo.description = description;
-        this.todos.push(todo);
-        return todo;
+    async findTodoById(id:number) {
+        const todos = await this.TodoRepository.find({id});
+        if (todos[0])
+          return todos[0];
+        throw new NotFoundException(`Le todo d'id ${id} n'est pas disponible`);
     }
 
-    deleteTodo(id: string ){
+    async findAllTodo(): Promise<TodoEntity[]> {
+        return await this.TodoRepository.find();
+    }
+
+
+    async addTodo(newTodo : CreateTodoDto): Promise<TodoEntity > {
+        const todo = await this.TodoRepository.create(newTodo);
+        return await this.TodoRepository.save(todo);
+    }
+
+    /*deleteFakeTodo(id:  number ){
         const todo = this.findTodoById(id);
         this.todos.splice(this.todos.indexOf(todo),1);
         return this.todos;
+    }*/
+    async deleteTodo(id:number ){
+        return await this.TodoRepository.softDelete(id)
     }
-
-    updateTodo(id: string, newTodo: UpdateTodoDto){
-        const {name, description, statut} = newTodo;
-        const todo= this.findTodoById(id);
-        todo.name = name? name : todo.name;
-        todo.description = description? description : todo.description;
-        todo.statut = statut? statut : todo.statut;
-        return this.todos;
+    async restoreTodo(id:string ){
+        return await this.TodoRepository.restore(id)
+    }
+    async updateTodo( id: string, newTodo: UpdateTodoDto): Promise<TodoEntity>{
+        const todo = await this.TodoRepository.preload({
+            id:+id,
+            ...newTodo
+        })
+        if(todo) {
+            new NotFoundException(`le todo d'id ${id} n'existe pas`);
+        }
+        return await this.TodoRepository.save(todo);
     }
 }
